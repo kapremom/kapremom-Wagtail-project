fafbb72488838de939415025c17effe26ca9fc8a
kapre <kapremom@gmail.com>
2020-07-18 18:00:24 -0400

    custom streamfield logic

--- flex/models.py
+++ flex/models.py
@@ -22,6 +22,7 @@ class FlexPage(Page):
             ("simple_richtext", blocks.SimpleRichTextBlock()),
             ("cards", blocks.CardBlock()),
             ("cta", blocks.CTABlock()),
+            ("button", blocks.ButtonBlock()),
         ],
         null=True,
         blank=True
